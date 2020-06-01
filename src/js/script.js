f5e6361d05dca7ca917af44f62af9e93f83363f2
mssffct <herooftheday82@mail.ru>
2020-06-01 17:03:22 +0400

    slider to be finished

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        // responsive: [
+        //     {
+        //     breakpoint: 768,
+        //     settings: {
+        //         arrows: false,
+        //         centerMode: true,
+        //         centerPadding: '40px',
+        //         slidesToShow: 3
+        //     }
+        //     },
+        //     {
+        //     breakpoint: 480,
+        //     settings: {
+        //         arrows: false,
+        //         centerMode: true,
+        //         centerPadding: '40px',
+        //         slidesToShow: 1
+        //     }
+        //     }
+        // ]
+    });
+});
